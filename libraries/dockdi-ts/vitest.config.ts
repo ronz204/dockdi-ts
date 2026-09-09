52e2cc70f5cc294b2621b2e737f27c7d5eecb1d8
roney ruiz <roneyruizrojas@gmail.com>
2026-09-09 15:13:57 -0600

    feat(ts): ad tspaths aliases to avoid relative imports

--- libraries/dockdi-ts/vitest.config.ts
+++ libraries/dockdi-ts/vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@source": "./source",
+      "@testing": "./testing",
+    },
+  },
   test: {
     include: ["source/**/*.test.ts", "testing/**/*.test.ts"],
     passWithNoTests: true,
